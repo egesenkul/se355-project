27fc654078c3f23dab156c041f884ee51faa3492
egesenkul <egesenkul35@hotmail.com>
2018-04-11 11:58:51 +0300

    Created a draft simple menu

---
+++ SE 355 SNAKE/Assets/menuButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuButtons : MonoBehaviour {
+
+	public void doQuit(){
+		Debug.Log ("quit!");
+		Application.Quit ();
+	}
+
+	public void startGame(){
+		Debug.Log ("oyunu aç!");
+		Application.LoadLevel ("GamePlay");
+	}
+}
