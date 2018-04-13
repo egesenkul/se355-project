56676da7e723699a8c49e90391b2252276f48306
egesenkul <egesenkul35@hotmail.com>
2018-04-13 12:32:10 +0300

    Pause Menu Added

---
+++ SE 355 SNAKE/Assets/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused;
+	public GameObject pauseMenuUI;
+	public GameObject menuButton;
+
+	void Start (){
+		menuButton = GameObject.Find ("ButtonPause");
+		GameIsPaused = false;
+	}
+
+	void Update () {
+		if (GameIsPaused) {
+			Pause ();
+		} else {
+			Resume ();
+		}
+	}
+
+	public void Resume(){
+		pauseMenuUI.SetActive (false);
+		Time.timeScale = 1; //freeze time in game
+		GameIsPaused = false;
+		menuButton.SetActive (true);
+	}
+
+	void Pause(){
+		pauseMenuUI.SetActive (true);
+		Time.timeScale = 0f; //freeze time in game
+	}
+
+	public void OpenPauseMenu(){
+		Debug.Log ("pause");
+		GameIsPaused = true;
+		menuButton.SetActive (false);
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+
+	public void OpenMainMenu(){
+		Application.LoadLevel ("Menu");
+	}
+}
--- SE 355 SNAKE/Assets/menuButtons.cs
+++ SE 355 SNAKE/Assets/menuButtons.cs
@@ -10,7 +10,6 @@ public class menuButtons : MonoBehaviour {
 	}
 
 	public void startGame(){
-		Debug.Log ("oyunu aç!");
 		Application.LoadLevel ("GamePlay");
 	}
 }
