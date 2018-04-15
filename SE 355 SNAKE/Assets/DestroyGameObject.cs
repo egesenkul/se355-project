5f7d2a1d6311919f4cda86592851090894c5dbd6
egesenkul <egesenkul35@hotmail.com>
2018-04-16 01:00:53 +0300

    Score & High Score added

--- SE 355 SNAKE/Assets/DestroyGameObject.cs
+++ SE 355 SNAKE/Assets/DestroyGameObject.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestroyGameObject : MonoBehaviour {
 	public SnakeScript snake_script;
 	public int i = 0;
+	public int score=0;
+	public int highScore;
+
+	public Text scoreUI;
 
 	public RandomCreateObject sn;
 	// Use this for initialization
 	void Start () {
 		snake_script = GameObject.Find ("Snake").GetComponent<SnakeScript> ();
+		highScore = PlayerPrefs.GetInt ("HighScore");
+		scoreUI.text = "Score: "+score.ToString ();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +26,11 @@ public class DestroyGameObject : MonoBehaviour {
 		foreach (GameObject item in Cubeler) {
 			if ((item.transform.position - transform.position).magnitude < 1.5f) {
 				Destroy (item);
+				score++;
+				scoreUI.text = "Score: "+score.ToString ();
+				if (score > highScore) {
+					PlayerPrefs.SetInt ("HighScore", score);
+				}
 				snake_script.speed += 0.25f;
 				sn.GenerateRandomObject(1);
 				if (i < 2) {
--- SE 355 SNAKE/Assets/menuButtons.cs
+++ SE 355 SNAKE/Assets/menuButtons.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class menuButtons : MonoBehaviour {
+
+	public Text hs;
+
+	void Start(){
+		hs.text = "High Score: "+PlayerPrefs.GetInt ("HighScore").ToString();
+	}
 
 	public void doQuit(){
 		Debug.Log ("quit!");
